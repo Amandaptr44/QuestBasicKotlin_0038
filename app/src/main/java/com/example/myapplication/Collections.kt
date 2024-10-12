657943a47bd7d20254d7372bd470961b18939138
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-10-12 17:09:15 +0700

    Tambahkan tampilan Read-Only dari daftar yang dapat diubah dengan shapesLocked

--- app/src/main/java/com/example/myapplication/Collections.kt
+++ app/src/main/java/com/example/myapplication/Collections.kt
@@ -27,4 +27,8 @@ fun ContohList() {
     //Mengubah data di dalam List Mutable
     shape[0] = "Oval"
     println(shape)
+
+    //List Read-Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
 }
