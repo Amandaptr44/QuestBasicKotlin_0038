1ee403ebaa8c5f38c157cff68c82c6e41a9645cb
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-10-12 17:06:33 +0700

    Mengubah data dari list Mutable

--- app/src/main/java/com/example/myapplication/Collections.kt
+++ app/src/main/java/com/example/myapplication/Collections.kt
@@ -23,4 +23,8 @@ fun ContohList() {
     //Menghapus data dari List Mutable
     shape.remove("Triangle")
     println(shape)
+
+    //Mengubah data di dalam List Mutable
+    shape[0] = "Oval"
+    println(shape)
 }
